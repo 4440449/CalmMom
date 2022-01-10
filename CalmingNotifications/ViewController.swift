902238db815d914617d284b878c027c76f3b192a
Max Terpugov <4440449@gmail.com>
2022-01-10 23:20:05 +0300

    Initial Commit

---
+++ CalmingNotifications/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CalmingNotifications
+//
+//  Created by Max on 10.01.2022.
+//  Copyright © 2022 Max. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
