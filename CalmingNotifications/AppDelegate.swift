b42fa0a44d61bca900486ce6451305291726e238
Max <4440449@gmail.com>
2022-07-19 14:23:05 +0300

    fixed code for supporting iOS 13* version

--- CalmingNotifications/AppDelegate.swift
+++ CalmingNotifications/AppDelegate.swift
@@ -8,27 +8,29 @@
 
 import UIKit
 import UserNotifications
+import PhotosUI
 
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .badge, .sound]) { isAllowed, error in
-//            print("\(isAllowed) ++++ \(error)")
+        UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .badge, .sound]) { isAllowed, error in }
+        if #available(iOS 14, *) {
+            PHPhotoLibrary.requestAuthorization(for: .addOnly) { status in }
+        } else {
+            PHPhotoLibrary.requestAuthorization { status in }
         }
         return true
     }
-
-    // MARK: UISceneSession Lifecycle
-
+    
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
         return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
     }
-
+    
     func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
     }
-
-
+    
+    
 }
 
